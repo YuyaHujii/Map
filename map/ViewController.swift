89873b13633bd7dc8172de8c93edfdee6674b883
yuuya <kaosu@yuuya-no-macbook-pro.local>
2015-11-15 16:21:47 +0900

    Initial Commit

---
+++ map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  map
+//
+//  Created by yuuya on 2015/11/15.
+//  Copyright © 2015年 yuuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
